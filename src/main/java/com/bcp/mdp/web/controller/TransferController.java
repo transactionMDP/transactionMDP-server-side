5fb6e50ed8b16173446345f5ca7895168df44778
chou-cha <47361850+chou-cha@users.noreply.github.com>
2019-04-24 16:15:11 +0100

    Update TransferController.java

--- src/main/java/com/bcp/mdp/web/controller/TransferController.java
+++ src/main/java/com/bcp/mdp/web/controller/TransferController.java
@@ -30,7 +30,6 @@ public class TransferController {
 	
 	@GetMapping("/{reference}")
 	public Transaction transactionByReference(@PathVariable String reference) {
-		System.out.println("okkkkkk");
 		return transferService.retrieveByReference(reference);
 		
 	}
@@ -39,24 +38,24 @@ public class TransferController {
 		return null; // transferService.retrieveTransactionsToExecuteToday();
 		
 	}
-	
-	@GetMapping("/cancel/{reference}")
+	@PutMapping("/cancel/{reference}")
 	public void cancelTransaction(@PathVariable String reference ) {
 		transferService.updateTransactionState(reference, "5000");
 	}
 	
-	@GetMapping("/refuse/{reference}")
+	@PutMapping("/refuse/{reference}")
 	public void refuseTransaction(@PathVariable String reference) {
 		transferService.updateTransactionState(reference, "4000");
 	}
 	
-	@GetMapping("/validate/{reference}")
+	@PutMapping("/validate/{reference}")
 	public void validateTransaction(@PathVariable String reference ) {
 		transferService.updateTransactionState(reference, "3000");
 	}
 	
-	@GetMapping("/sendToCtn/{reference}")
+	@PutMapping("/sendToCtn/{reference}")
 	public void updateTransactionState(@PathVariable String reference ) {
 		transferService.updateTransactionState(reference, "2000");
 	}
+	//HJKLM%
 }
